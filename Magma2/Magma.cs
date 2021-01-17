da1b14bdd4903ec190fd269eaa2ad759c0077324
drmixvm <drmixvm@DESKTOP-T3TQNU6>
2021-01-17 21:49:45 +0500

    Добавьте файлы проекта.

---
+++ Magma2/Magma.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magma2
+{
+    public static class Magma
+    {
+        public static uint HexToUint(string hex)
+        {
+            return Convert.ToUInt32(hex, 16);
+        }
+
+        public static string UintToHex(uint a)
+        {
+            return a.ToString("X");
+        }
+        //4 лр Блок замены в шифре ГОСТ Р 34.12-2015 с длиной блока 64 бита (Магма)
+        public static uint t(uint block)
+        {
+            uint[,] p =
+            {
+                {12, 4, 6, 2, 10, 5, 11, 9, 14, 8, 13, 7, 0, 3, 15, 1 },
+                {6, 8, 2, 3, 9, 10, 5, 12, 1, 14, 4, 7, 11, 13, 0, 15 },
+                {11, 3, 5, 8, 2, 15, 10, 13, 14, 1, 7, 4, 12, 9, 6, 0 },
+                {12, 8, 2, 1, 13, 4, 15, 6, 7, 0, 10, 5, 3, 14, 9, 11 },
+                {7, 15, 5, 10, 8, 1, 6, 13, 0, 9, 3, 14, 11, 4, 2, 12 },
+                {5, 13, 15, 6, 9, 2, 12, 10, 11, 7, 8, 1, 4, 3, 14, 0 },
+                {8, 14, 2, 5, 6, 9, 1, 12, 15, 4, 11, 0, 13, 10, 3, 7 },
+                {1, 7, 14, 13, 0, 5, 8, 3, 4, 15, 10, 6, 9, 12, 11, 2 }
+            };
+            uint tmp;
+            for (int i = 7; i >= 0; i--)
+            {
+                tmp = block >> 28;
+                block = block & (uint)(Math.Pow(2, 32) -
+                    Math.Pow(2, 31) - Math.Pow(2, 30) - Math.Pow(2, 29) - Math.Pow(2, 28) - 1); //-старшие 4 бита
+                block = block << 4;
+                block |= p[i, (int)tmp];
+            }
+            return block;
+        }
+    }
+}
---
+++ Magma2/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+/*5 лр Процедуры разворачивания ключа, 
+зашифрования и расшифрования блока в шифре 
+ГОСТ Р 34.12-2015 с длиной блока 64 бита (Магма).*/
+namespace Magma2
+{
+    class Program
+    {
+        static uint CycleShiftLeft(uint n, int step)
+        {
+            uint n1, n2;
+            uint mask = (uint)(Math.Pow(2, 32) - Math.Pow(2, 31) - 1);
+            for (int i = 0; i < step; i++)
+            {
+                n1 = (n & mask) << 1;
+                n2 = n >> 32 - 1;
+                n = n1 | n2;
+            }
+            return n;
+        }
+        static uint g(uint k, uint a0)
+        {
+            UInt32 result = a0 + k;
+            result = Magma.t(result);
+            result = CycleShiftLeft(result, 11);
+
+            return result;
+        }
+
+        static uint[] Crypt(uint a0, uint a1, uint[] _8bitkeys)
+        {
+            uint tmp;
+            uint[] _8bitkeysrev = _8bitkeys.Reverse().ToArray();
+
+            for (int i = 0; i < 24; i++)
+            {
+                tmp = a0;
+                a0 = g(_8bitkeys[i % 8], a0);
+                a0 = a0 ^ a1;
+                a1 = tmp;
+            }
+            for (int i = 24; i < 32; i++)
+            {
+                tmp = a0;
+                a0 = g(_8bitkeysrev[i % 8], a0);
+                a0 = a0 ^ a1;
+                a1 = tmp;
+            }
+            return new uint[] { a1, a0 };
+        }
+
+
+        static uint[] Decrypt(uint a0, uint a1, uint[] _8bitkeys)
+        {
+            uint tmp;
+            uint[] _8bitkeysrev = _8bitkeys.Reverse().ToArray();
+
+            for (int i = 0; i < 8; i++)
+            {
+                tmp = a0;
+                a0 = g(_8bitkeys[i % 8], a0);
+                a0 = a0 ^ a1;
+                a1 = tmp;
+            }
+            for (int i = 8; i < 32; i++)
+            {
+                tmp = a0;
+                a0 = g(_8bitkeysrev[i % 8], a0);
+                a0 = a0 ^ a1;
+                a1 = tmp;
+            }
+            return new uint[] { a1, a0 };
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите ключ в 16чной системе счисления (256 бит): ");
+            string strkey = Console.ReadLine();
+            BigInteger key = BigInteger.Parse(strkey, System.Globalization.NumberStyles.HexNumber);
+
+            uint[] _8bitkeys = new uint[8];
+
+            for (int i = 0; i < _8bitkeys.Length; i++)
+            {
+                _8bitkeys[7 - i] = (uint)(key & 0xffffffff); //последние 32 бита
+                key = key >> 32;
+            }
+            Console.Write("Введите сообщение в 16чной системе счисления (64 бита): ");
+            string stra = Console.ReadLine();
+            BigInteger a = BigInteger.Parse(stra, System.Globalization.NumberStyles.HexNumber);
+
+            uint a0 = (uint)(a & 0xffffffff);
+            a = a >> 32;
+            uint a1 = (uint)(a & 0xffffffff);
+
+            uint[] cres = Crypt(a0, a1, _8bitkeys);
+            a0 = cres[0];
+            a1 = cres[1];
+
+            Console.WriteLine("Результат шифрования: " + a1.ToString("X") + " " + a0.ToString("X"));
+
+            uint[] dres = Decrypt(a0, a1, _8bitkeys);
+            a0 = dres[0];
+            a1 = dres[1];
+
+            Console.WriteLine("Результат дешифрования: " + a1.ToString("X") + " " + a0.ToString("X"));
+
+            Console.ReadKey();
+        }
+    }
+}
